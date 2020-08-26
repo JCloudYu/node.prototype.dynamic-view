eeef22c5954a72b1630d0e94495b66b005d3b175
Cloud Yu <j.cloud.yu@gmail.com>
2020-08-26 18:37:07 +0800

    Update dependent libraries

--- kernel-info.esm.js
+++ kernel-info.esm.js
@@ -1,12 +1,12 @@
 import fs from "fs";
 import os from "os";
 import path from "path";
-import {Beson} from "beson/beson.esm.js";
+import Beson from "beson";
 import {Version} from "/kernel/version.esm.js";
 
 
 // Arch kernel - https://github.com/JCloudYu/node.prototype.kernel/
-const KernelArchVersion = [{identifier: "node.prototype.kernel", version: "1.1.6"}];
+const KernelArchVersion = [{identifier: "node.prototype.kernel", version: "1.2.0"}];
 
 // other application arch type can add additional versions like following line
 // KernelArchVersion.push({identifier: "other_arch_versions", version: "0.9.0"});
