36019a9a0b7adc73e13e4107e36720bae604af6e
Cloud Yu <j.cloud.yu@gmail.com>
2020-09-03 14:47:39 +0800

    Update version

--- kernel-info.esm.js
+++ kernel-info.esm.js
@@ -9,7 +9,7 @@ import {Version} from "/kernel/version.esm.js";
 const KernelArchVersion = [{identifier: "node.prototype.kernel", version: "1.3.0"}];
 
 // Arch api - https://github.com/JCloudYu/node.prototype.api/
-KernelArchVersion.push({identifier: "node.prototype.api", version: "1.1.0"});
+KernelArchVersion.push({identifier: "node.prototype.api", version: "1.2.0"});
 
 // other application arch type can add additional versions like following line
 // KernelArchVersion.push({identifier: "other_arch_versions", version: "0.9.0"});
