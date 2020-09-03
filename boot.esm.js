234e9ad46fae929276ffa5ffb5f117ed3e0edbda
Cloud Yu <j.cloud.yu@gmail.com>
2020-09-03 23:10:20 +0800

    Add fast boot scripts for both linux and windows environment

--- boot.esm.js
+++ boot.esm.js
@@ -1,55 +1,56 @@
 import "extes";
 import ColorCode from "/kernel/term-code.js";
 
-const N=()=>{};
-
-
-
-process
-.on('unhandledRejection',(rej)=>{
-	console.error( `${ColorCode.LIGHT_RED}Receiving unhandled rejection! Exiting...${ColorCode.RESET}\n`, rej);
-	process.exit(1);
-})
-.on('uncaughtException',(e)=>{
-	console.error(`${ColorCode.LIGHT_RED}Receiving uncaught exception! Exiting...${ColorCode.RESET}\n`, e);
-	process.exit(1);
-})
-.on('SIGINT',N).on('SIGTERM',N);
-
-
-
 const DEFAULT_BOOT_MAP = {
 	version: "/kernel/boot-scripts/version.esm.js",
 	update: "/kernel/boot-scripts/update.esm.js",
 	run: "/kernel/boot-scripts/run-script.esm.js"
 };
+const CLEANUP_QUEUE = [];
+const N=()=>{};
+const ERR=(e)=>{
+	console.error(`${ColorCode.LIGHT_RED}Receiving unhandled errors! Exiting...${ColorCode.RESET}\n`, e);
+	process.exit(1);
+};
+
+
+
+process
+.once('cleanup', async()=>{
+	for(const cleanup_callback of CLEANUP_QUEUE) {
+		await Promise.resolve(cleanup_callback()).catch((e)=>console.error(e));
+	}
+})
+.on('unhandledRejection',ERR).on('uncaughtException',ERR)
+.on('SIGINT',N).on('SIGTERM',N);
+
+
+
+/**
+ * @typedef {String[]} NodeJS.Process.exec_args
+**/
 
 (async()=>{
 	// INFO: Idle everything to hoist warning verbose
 	await setTimeout.idle(100);
 	
-	// INFO: Decide boot script
 	const [boot_cmd='', ..._argv] = process.argv.slice(2);
-	process._argv = _argv;
+	process.exec_args = _argv;
 	
 	
-	// INFO: Collect information about current runtime environment
+	// Collect information about current runtime environment
 	console.error( `${ColorCode.DARK_GRAY}Obtaining kernel info...${ColorCode.RESET}` );
 	const ProjectInfo = await import('/kernel-info.esm.js').then(async({Init, _ProjectInfo})=>{await Init(); return _ProjectInfo;});
 	
-	// INFO: Load environmental configurations
+	// Load environmental configurations
 	console.error( `${ColorCode.DARK_GRAY}Loading configurations...${ColorCode.RESET}` );
 	await import( "/kernel/config.esm.js" ).then(({Init})=>Init());
 	
-	// INFO: Load environmental configurations
+	// Load environmental configurations
 	console.error( `${ColorCode.DARK_GRAY}Loading configurations...${ColorCode.RESET}` );
 	await import( "/kernel/runtime.esm.js" ).then(({Init})=>Init());
 	
-	// INFO: Initialize remaining contents
-	console.error( `${ColorCode.DARK_GRAY}Loading configurations...${ColorCode.RESET}` );
-	await import( "/boot-init.esm.js" ).then(({Init})=>Init?Init():null).catch(()=>{});
-	
-	// INFO: Expose modules as global variables
+	// Expose modules as global variables
 	const global_map = Object.assign({}, ProjectInfo.expose_global||{});
 	for(const [var_name, module_path] of Object.entries(global_map)) {
 		if ( module_path === "os" && var_name === "os" ) continue;
@@ -60,7 +61,11 @@ const DEFAULT_BOOT_MAP = {
 		}
 	}
 	
-	// INFO: Detect boot script
+	// Initialize dynamic contents
+	console.error( `${ColorCode.DARK_GRAY}Loading configurations...${ColorCode.RESET}` );
+	await import( "/boot-init.esm.js" ).then(({Init})=>Init?Init():null).catch(()=>{});
+	
+	// Detect boot script
 	const boot_map = Object.assign({main:"/index.esm.js"}, ProjectInfo.kernel_script_map||{}, DEFAULT_BOOT_MAP);
 	const command = boot_cmd||'main';
 	const boot_script = boot_map[command];
@@ -71,9 +76,9 @@ const DEFAULT_BOOT_MAP = {
 	}
 	
 	await import(boot_script);
-})()
-.catch((e)=>{
-	// NOTE: Force to cast unexpectedly rejected promises into exceptions
-	setTimeout(()=>{throw e;});
-});
+	
+	
+	// Make sure the cleanup call is registered as later as possible
+	process.on('SIGTERM', ()=>process.emit('cleanup'));
+})().catch(ERR);
 
